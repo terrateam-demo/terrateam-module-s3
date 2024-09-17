43ea8df0fa719994f15bb7a5784de061bd269309
Bender Bending Rodríguez <165169475+bender2352@users.noreply.github.com>
2024-09-17 16:22:57 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,13 @@
-resource "null_resource" "foo" {
-  provisioner "local-exec" {
-    command = "echo foo"
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = var.bucket_name
+  acl    = "private"
+
+  tags = {
+    Name        = var.bucket_name
+    Environment = var.environment
   }
 }
